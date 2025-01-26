4db5b982f20a89099344b300ec4e7db7914a52e8
Anand Kumar Sinha <109061880+AnandKumarSinha07@users.noreply.github.com>
2025-01-26 14:28:51 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,12 +1,15 @@
-export const URL="https://devtinder-evho.onrender.com"
+    export const URL="https://devtinder-evho.onrender.com"
 
-export const LOGIN_URL="http://localhost:7777/login"
-export const PROFILE_URL="http://localhost:7777/profile/view"
-export const LOGOUT_URL="http://localhost:7777/logout"
-export const FEED_URL="http://localhost:7777/user/feed"
-export const EDIT_URL="http://localhost:7777/profile/edit"
-export const USER_CONNECTION="http://localhost:7777/user/connection"
-export const REQUEST_RECEIVED="http://localhost:7777/user/requests/received"
-export const BASE_URL="http://localhost:7777";
-export const SEND_REQUEST="http://localhost:7777/request/send"
-export const SIGNUP="http://localhost:7777/signup"; 
+    export const LOGIN_URL="/api/login"
+    export const PROFILE_URL="/api/view"
+    export const LOGOUT_URL="/api/logout"
+    export const FEED_URL="/api/feed"
+    export const EDIT_URL="/api/profile/edit"
+    export const USER_CONNECTION="/api/user/connection"
+    export const REQUEST_RECEIVED="/api/user/requests/received"
+    export const BASE_URL="/api";
+    export const SEND_REQUEST="/api/request/send"
+    export const SIGNUP="/api/signup";  
+
+    // export const BASE="http://localhost:7777" ;
+    // export const new_="/api"
